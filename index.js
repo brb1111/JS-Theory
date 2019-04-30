8c09bfea3f1998fdbc5472523157e0876ab1d1e1
OHzz <brb1111@naver.com>
2019-05-01 00:58:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ const wat = 666;
 /* Float
 cosnt wat = 55.1;
 */
+
+/*camel case = 변수명에서 스페이스를 써야하는 곳은 변수명에서 대문자로 입력
+ex)
+const what to do = "왓투두"; (x)
+const whatToDo ="왓투두";
+이게 프로그래머의 암묵적인 규칙이랍니다.
+*/
+/*
+배열
+const something = "Something"
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", something];
+*/
