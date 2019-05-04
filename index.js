9380907eaf8a09cd8bee18ac4aa132edbd7cd3a7
OHzz <brb1111@naver.com>
2019-05-04 21:10:58 +0900

    function generate with if else DOM

--- index.js
+++ index.js
@@ -76,9 +76,31 @@ title.innerHTML = "Hi! From JS";
 title.style.color = "red";
 document.title = 'I own you know';
 */
+//event function
+/* 
 const title = document.querySelector("#title");
 
 function handleClick(){
   title.style.color = "blue"
 }
 window.addEventListener("click", handleClick);
+*/
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick(){
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR){
+    title.style.color = OTHER_COLOR;
+  }else{
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init(){
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init();
